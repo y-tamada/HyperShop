a2e2a0cf579d943950673d223c698cc783181d67
tamada youichi <siratama5080@gmail.com>
2016-05-27 08:58:31 +0900

    開発

--- hypershop/src/main/java/com/hypershop/app/controller/TopController.java
+++ hypershop/src/main/java/com/hypershop/app/controller/TopController.java
@@ -1,17 +1,21 @@
 package com.hypershop.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TopController {
 	
-	@RequestMapping("/top")
+	private static final Logger logger = LoggerFactory.getLogger(TopController.class);
+	
+	@RequestMapping("/")
 	public String init(Model model){
 		String message = "hello world!";
 		model.addAttribute("message", message);
+		logger.info("log test!");
 		return "index";
 	}
 }
