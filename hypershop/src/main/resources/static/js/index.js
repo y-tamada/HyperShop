5ddc117451754860135a538681f3b82d4327091b
tamada youichi <siratama5080@gmail.com>
2016-06-01 01:01:56 +0900

    デザイン変更

--- hypershop/src/main/resources/static/js/index.js
+++ hypershop/src/main/resources/static/js/index.js
@@ -4,7 +4,14 @@
 
 $(document).ready(function(){
 	
-	
+	//ratringStar
+			$(function() {
+					$('select[name=ratingStar]').barrating({
+						theme: 'fontawesome-stars',
+						readonly: true,
+						showSelectedRating: true
+					});
+			 });
 	
 	$("#productSearch").on("click", function(){
 		$.ajax({
@@ -13,38 +20,39 @@ $(document).ready(function(){
 			data: { 'keyword' : $("input[name=keyword]").val() },
 			async: false,
 			success: function(data){
+				$("#productListArea ul").remove();
 				$("#productListArea").append(data);
 			}
 		});
 		
 		$(".productInfo").each(function(){
-			var index = $(this).attr("data-value");
-			switch(index % 4){
-			case 0:
-				$(this).addClass("panelBlue");
-				break;
-			case 1:
-				$(this).addClass("panelGreen");
-				break;
-			case 2:
-				$(this).addClass("panelYellow");
-				break;
-			case 3:
-				$(this).addClass("panelRed");
-				break;
-			default:
-				$(this).addClass("panelBlue");
-			break;
-			}
+//			var index = $(this).attr("data-value");
+//			switch(index % 4){
+//			case 0:
+//				$(this).addClass("panelBlue");
+//				break;
+//			case 1:
+//				$(this).addClass("panelGreen");
+//				break;
+//			case 2:
+//				$(this).addClass("panelYellow");
+//				break;
+//			case 3:
+//				$(this).addClass("panelRed");
+//				break;
+//			default:
+//				$(this).addClass("panelBlue");
+//			break;
+//			}
 			
 			//ratringStar
 			$(function() {
-			      $('select[name=ratingStar]').barrating({
-			        theme: 'fontawesome-stars',
-			        readonly: true,
-			        showSelectedRating: true
-			      });
-			   });
+					$('select[name=ratingStar]').barrating({
+						theme: 'fontawesome-stars',
+						readonly: true,
+						showSelectedRating: true
+					});
+			 });
 		});
 	});
 });
